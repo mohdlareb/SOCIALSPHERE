de870c14be827172512e125031858245148a515c
mohdlareb <larebtaqi@gmail.com>
2026-06-26 01:53:20 +0530

    basic

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -48,18 +48,21 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOrigins(List.of(
+        	    "http://localhost:5173",
+        	    "https://socialsphere-inky.vercel.app"
+        	));
 
-        configuration.setAllowedMethods(List.of(
-                "GET",
-                "POST",
-                "PUT",
-                "DELETE",
-                "OPTIONS"
-        ));
+        	configuration.setAllowedMethods(List.of(
+        	    "GET",
+        	    "POST",
+        	    "PUT",
+        	    "DELETE",
+        	    "OPTIONS"
+        	));
 
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(false);
+        	configuration.setAllowedHeaders(List.of("*"));
+        	configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
